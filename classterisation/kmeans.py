73e3b5f814c87f3662c84447e90208937fa9f07e
svetlanastojadinov <stojadinov.svetlana@gmail.com>
2018-03-06 20:34:08 +0100

    kmeans

--- classterisation/kmeans.py
+++ classterisation/kmeans.py
@@ -7,4 +7,16 @@ from sklearn.cross_validation import train_test_split
 # normalization
 data = import_data.import_data_for_kmeans()
 data = scale(data)
-# print(data)
+
+print("Size of data:",data.size)
+
+kmeans=KMeans(n_clusters=5,max_iter=50).fit(data)
+labels=kmeans.predict(data)	#index of the cluster each sample belongs to
+print("Size of leng labels:",labels.size)
+
+num_clus=[0, 0, 0, 0, 0]
+for i in labels:
+	num_clus[labels[i]]+=1
+
+for i in range(5):
+	print("Cluster",i,"has", num_clus[i],"items")
